7016c685310a9ff9e90924a3a709feca03487407
tsaniamagistra <123210090@student.upnyk.ac.id>
2023-12-06 01:38:57 +0700

    menambahkan histori bahaya dan siaga

--- web_conf/src/webapi/handler.js
+++ web_conf/src/webapi/handler.js
@@ -63,8 +63,60 @@ const viewLogHandler = async (request, h) => {
   }
 };
 
+const viewBahayaHandler = async (request, h) => {
+  try {
+    const [rows] = await dbPool.query(
+      'SELECT * FROM log WHERE level = ? ORDER BY log_time DESC LIMIT 4', ['bahaya']
+    );
+
+    return {
+      status: 'success',
+      data: {
+        logs: rows,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching logs:', error);
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal mengambil data log dengan level bahaya dari database',
+    });
+    response.code(500);
+    return response;
+  }
+};
+
+const viewSiagaHandler = async (request, h) => {
+  try {
+    const [rows] = await dbPool.query(
+      'SELECT * FROM log WHERE level = ? ORDER BY log_time DESC LIMIT 4', ['siaga']
+    );
+
+    return {
+      status: 'success',
+      data: {
+        logs: rows,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching logs:', error);
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal mengambil data log dengan level siaga dari database',
+    });
+    response.code(500);
+    return response;
+  }
+};
+
 const checkConnectionHandler = (request, h) => {
   return 'Hello World!';
 };
 
-module.exports = { insertLogHandler, viewLogHandler, checkConnectionHandler };
+module.exports = {
+  insertLogHandler,
+  viewLogHandler,
+  checkConnectionHandler,
+  viewBahayaHandler,
+  viewSiagaHandler,
+};
--- web_conf/src/webapi/routes.js
+++ web_conf/src/webapi/routes.js
@@ -1,19 +1,35 @@
-const { insertLogHandler, viewLogHandler, checkConnectionHandler } = require('./handler');
+const {
+  insertLogHandler,
+  viewLogHandler,
+  checkConnectionHandler,
+  viewBahayaHandler,
+  viewSiagaHandler, } = require('./handler');
 
-module.exports = [
-  {
-    method: 'POST',
-    path: '/insert-log',
-    handler: insertLogHandler
-  },
-  {
-    method: 'GET',
-    path: '/view-log',
-    handler: viewLogHandler
-  },
-  {
-    method: 'GET',
-    path: '/check-connection',
-    handler: checkConnectionHandler,
-  },
-];
+  module.exports = [
+    {
+      method: 'POST',
+      path: '/insert-log',
+      handler: insertLogHandler
+    },
+    {
+      method: 'GET',
+      path: '/view-log',
+      handler: viewLogHandler
+    },
+    {
+      method: 'GET',
+      path: '/view-bahaya',
+      handler: viewBahayaHandler
+    },
+    {
+      method: 'GET',
+      path: '/view-siaga',
+      handler: viewSiagaHandler
+    },
+    {
+      method: 'GET',
+      path: '/check-connection',
+      handler: checkConnectionHandler,
+    },
+  ];
+  
